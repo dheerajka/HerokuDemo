e084928cca27380fcaffa3a0f6f71495b418c13b
dheerajka <9442995+dheerajka@users.noreply.github.com>
2017-11-09 20:40:40 +0530

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -47,7 +47,7 @@ function showSessionDetails(sessionId) {
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
                 '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
-            '<h1 class="title">Sessions</h1>' +
+            '<h1 class="title">Dheeraj Sessions</h1>' +
                 '</header>' +
                 '<div class="content">' +
                     '<div class="card">' +
